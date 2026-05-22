9b1c473ea8abcea94fac28648c05874cd101fb97
Bfp 24 <info@bfp24.com>
2026-05-22 09:52:10 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Coolify Test erfolgreich deployed 🚀');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server läuft auf Port ${PORT}`);
+});
+
